12810f358e5da349449d6db08675e2ebe66c0bb8
Sujai123 <sujaibeniks001@gmail.com>
2025-04-23 19:28:05 +0530

    added test for 1st use case

--- test/assessment_1.test.js
+++ test/assessment_1.test.js
@@ -1,7 +1,22 @@
 const {add} = require('../src/assessment_1.js');
 
 describe('#add', () => {
-  it('', () => {
-    
-  });
+  describe('when the input is empty', () => {
+    it('should return 0', () => {
+      expect(add('')).toBe(0);
+    });
+  })
+
+  describe('when the input is a single number', () => {
+    it('should return the number itself', () => {
+      expect(add('5')).toBe(5);
+    });
+  })
+
+  describe('when the input is a list of numbers', () => {
+    it('should return the sum of the numbers', () => {
+      expect(add('1,2,3')).toBe(6);
+    });
+  })
+
 })
