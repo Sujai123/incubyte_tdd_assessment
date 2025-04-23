9d490bb0ed8ffd84cf48359b61bff4a10377fa4f
Sujai123 <sujaibeniks001@gmail.com>
2025-04-23 19:19:55 +0530

    method outline

---
+++ app.js
@@ -0,0 +1,3 @@
+const { add } = import('./src/assesment_1.js');
+
+console.log(add([1, 2, 3, 4, 5]));
---
+++ src/assesment_1.js
@@ -0,0 +1,13 @@
+
+/**
+ * 
+ * @param {*} numbers string of numbers separated by delimiter
+ * @returns sum of numbers
+ */
+const add = (numbers) => {
+  return 0;
+}
+
+module.exports = {
+  add
+}
