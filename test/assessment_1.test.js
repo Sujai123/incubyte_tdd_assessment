3cbff2ac2666fbc487cfdc58b4326c2e28bc16e2
Sujai123 <sujaibeniks001@gmail.com>
2025-04-23 19:37:13 +0530

    added logic for 2nd use case

--- test/assessment_1.test.js
+++ test/assessment_1.test.js
@@ -1,3 +1,4 @@
+const { describe } = require('yargs');
 const {add} = require('../src/assessment_1.js');
 
 describe('#add', () => {
@@ -13,9 +14,25 @@ describe('#add', () => {
     });
   })
 
-  describe('when the input is a list of numbers', () => {
+  describe('when the input is a list of numbers separated by commas', () => {
     it('should return the sum of the numbers', () => {
       expect(add('1,2,3')).toBe(6);
+    });
+
+    it('should return the sum of the numbers', () => {
+      expect(add('1,2,,3')).toBe(6);
+    });
+  })
+
+  describe('when the input is a list of numbers separated by new lines', () => {
+    it('should return the sum of the numbers', () => {
+      expect(add('1\n2,3')).toBe(6);
+    });
+    it('should return the sum of the numbers with different delimiters', () => {
+      expect(add('1\n2,3\n4')).toBe(10);
+    });
+    it('should return the sum of the numbers with different delimiters and empty lines', () => {
+      expect(add('1\n\n2,3\n4')).toBe(10);
     });
   })
 
