f02deb8f9e9dcd3359f4cb7bfef5b91a264e263d
Sujai123 <sujaibeniks001@gmail.com>
2025-04-23 19:33:50 +0530

    added logic for 1st use case

--- src/assessment_1.js
+++ src/assessment_1.js
@@ -5,7 +5,16 @@
  * @returns sum of numbers
  */
 const add = (numbers) => {
-  return 0;
+  const arrayOfNumbers = numbers.split(',');
+  let sum = 0;
+  for (let i = 0; i < arrayOfNumbers.length; i++) {
+    const num = parseInt(arrayOfNumbers[i]);
+    if (isNaN(num)) {
+      continue;
+    }
+    sum += num;
+  }
+  return sum;
 }
 
 module.exports = {
